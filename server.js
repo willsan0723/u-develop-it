5e15acb616a612c7ab2f133661a25740b5353971
willsan0723 <william.santee@gmail.com>
2020-10-15 18:01:42 -0600

    add post route

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const inputCheck = require('./utils/inputCheck');
 const sqlite3 = require('sqlite3').verbose();
 const express = require('express');
 
@@ -65,6 +66,31 @@ app.delete('/api/candidate/:id', (req, res) => {
         res.json({
             message: 'successfully deleted',
             changes: this.changes
+        });
+    });
+});
+
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+              VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    // ES5 function, not arrow function, to use `this`
+    db.run(sql, params, function (err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: body,
+            id: this.lastID
         });
     });
 });
